bb258fddfa67381c08cb3684a7268548f827b604
Kyle Highsmith <kyle@Kyles-Air.technicolor.net>
2022-06-27 09:48:35 +0200

    testing connection

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,7 @@ router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
 router.use((req, res) => {
-  res
-    .status(HTTP_BAD_REQUEST)
-    .send({ message: 'The requested resource was not found' });
+  res.status(HTTP_BAD_REQUEST).send({ message: 'BAD REQUEST --- TESTING' });
 });
 
 module.exports = router;
